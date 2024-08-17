f06e809889cbf510188ef371a3f4918d94986a4e
MilesJHansen <milesjhansen@gmail.com>
2024-08-18 00:26:39 +0800

    more Ooze setup

--- Assets/Scripts/VelocityWarp.cs
+++ Assets/Scripts/VelocityWarp.cs
@@ -7,20 +7,34 @@ public class VelocityWarp : MonoBehaviour
     private Material mat;
     private Vector3 previousPosition;
     private Vector3 velocityWarp;
+    [SerializeField] private float FPS;
+
     void Start()
     {
         mat = GetComponent<MeshRenderer>().material;
         previousPosition = transform.position;
         velocityWarp = Vector3.zero;
+        StartCoroutine(MaterialTimer());
     }
     
-    void Update()
+    void UpdateMaterialValues()
     {
         Vector3 velocity = transform.position - previousPosition;
-        velocityWarp = Vector3.Lerp(velocityWarp, velocity, 0.01f);
+        velocityWarp = Vector3.Lerp(velocityWarp, velocity, 0.1f);
         
         mat.SetVector("_VelocityWarp", velocityWarp);
         mat.SetVector("_Velocity", velocity);
+        mat.SetVector("_WorldPosition", transform.position);
+        mat.SetFloat("_GameTime", Time.time);
         previousPosition = transform.position;
+    }
+
+    IEnumerator MaterialTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f/FPS);
+            UpdateMaterialValues();
+        }
     }
 }
