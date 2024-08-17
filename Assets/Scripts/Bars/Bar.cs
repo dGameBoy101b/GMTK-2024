bf78ee66a16768847e7197eb5b33e972dd238792
dGameBoy101b <rhysmader@gmail.com>
2024-08-17 18:46:07 +0800

    Added update vlaue event to bar.

--- Assets/Scripts/Bars/Bar.cs
+++ Assets/Scripts/Bars/Bar.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public abstract class Bar : MonoBehaviour
 {
 	[Tooltip("The slider to control")]
 	public Slider Slider;
+
+	[Tooltip("Invoked when the value is updated.\nParameter is the current value")]
+	public UnityEvent<float> OnUpdateValue = new();
 
 	public abstract float Minimum { get; }
 	public abstract float Maximum { get; }
@@ -14,12 +18,15 @@ public abstract class Bar : MonoBehaviour
 	{
 		this.Slider.minValue = this.Minimum;
 		this.Slider.maxValue = this.Maximum;
+		Debug.Log($"Range updated: {this.Minimum} - {this.Maximum}", this);
 		this.UpdateValue();
 	}
 
 	public void UpdateValue()
 	{
 		this.Slider.value = this.Value;
+		Debug.Log($"Value updated: {this.Value}", this);
+		this.OnUpdateValue.Invoke(this.Value);
 	}
 
 	private void Awake()
