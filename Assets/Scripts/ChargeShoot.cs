d29d8406dafd0f3b1f96e9713aadf3e7d9868b46
dGameBoy101b <rhysmader@gmail.com>
2024-08-17 22:30:08 +0800

    Extracted charge shoot from shoot. Added cooldown to shoot.

---
+++ Assets/Scripts/ChargeShoot.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ChargeShoot : MonoBehaviour
+{
+	[Tooltip("The shoot component to control")]
+	public Shoot Shoot;
+
+	[Tooltip("The maximum power this can charge up to")]
+	[Min(0)]
+	public float MaxPower = 10f;
+
+	[Tooltip("How quickly power can charge up")]
+	[Min(0)]
+	public float ChargeRate = 1f;
+
+	[Tooltip("How quickly shots should cool down")]
+	public float BaseCooldown = 1f;
+
+	[Tooltip("The multiplier applied to the power before adding to the base cooldown")]
+	public float PowerCooldownCoefficient = 1f;
+
+	[Tooltip("The cool down duration calculated for the previous shot")]
+	public float LastCooldown;
+
+	[Tooltip("Invoked every frame while this is charging.\nParameter is current charge amount")]
+	public UnityEvent<float> OnChargeUp = new();
+
+	[Tooltip("Invoked every frame while this cools down.\nParameter is current charge amount")]
+	public UnityEvent<float> OnCoolDown = new();
+
+	[SerializeField]
+	[Tooltip("The current amount of power this has charged")]
+	private float _currentPower = 0f;
+	public float CurrentPower
+	{
+		get => this._currentPower;
+		set
+		{
+			value = Mathf.Clamp(value, 0f, this.MaxPower);
+			var old = this._currentPower;
+			this._currentPower = value;
+			if (old < value)
+				this.OnChargeUp.Invoke(value);
+		}
+	}
+
+	public void UpdateCharge(float delta_time)
+	{
+		if (this.Shoot.IsCooling)
+			this.CurrentPower -= this.BaseCooldown * delta_time;
+		else if (this.IsCharging)
+			this.CurrentPower += this.ChargeRate * delta_time;
+	}
+
+	public void Shot()
+	{
+		if (this.Shoot.IsCooling)
+			return;
+		this.LastCooldown = this.CurrentPower * this.PowerCooldownCoefficient + this.BaseCooldown;
+		this.Shoot.Cooldown = this.LastCooldown;
+		this.Shoot.Shot();
+	}
+
+	[Tooltip("Whether this should be charging a shot")]
+	private bool _isCharging = false;
+	public bool IsCharging
+	{
+		get => this._isCharging;
+		set
+		{
+			var old = this._isCharging;
+			this._isCharging = value;
+			if (old && !value)
+				this.Shot();
+		}
+	}
+
+	private void Update()
+	{
+		this.UpdateCharge(Time.deltaTime);
+	}
+}
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -3,45 +3,11 @@ using UnityEngine.Events;
 
 public class Shoot : MonoBehaviour
 {
-	#region Power Charging
-	[Tooltip("The maximum power this can charge up to")]
-	[Min(0)]
-	public float MaxPower = 10f;
-
-	[Tooltip("How quickly power can charge up")]
-	[Min(0)]
-	public float ChargeRate = 1f;
-
-	[Tooltip("Invoked every frame while this is charging.\nParameter is current charge amount")]
-	public UnityEvent<float> OnChargeUp = new();
-
-	[SerializeField]
-	[Tooltip("The current amount of power this has charged")]
-	private float _currentPower = 0f;
-	public float CurrentPower
-	{
-		get => this._currentPower;
-		set
-		{
-			value = Mathf.Clamp(value, 0f, this.MaxPower);
-			var old = this._currentPower;
-			this._currentPower = value;
-			if (old < value)
-				this.OnChargeUp.Invoke(value);
-		}
-	}
-
-	public void UpdateCharge(float delta_time)
-	{
-		if (!this.IsCharging)
-			return;
-		this.CurrentPower += this.ChargeRate * delta_time;
-	}
-	#endregion
-
-	#region Shooting
 	[Tooltip("The aiming component used to determine shot direction")]
 	public Aim Aim;
+
+	[Tooltip("The power to shoot bullets with")]
+	public float Power = 1;
 
 	[Tooltip("The bullet to instantiate when shooting")]
 	public Bullet Bullet;
@@ -49,33 +15,30 @@ public class Shoot : MonoBehaviour
 	[Tooltip("Invoked when this shoots.\nParameter is shot power")]
 	public UnityEvent<float> OnShot = new();
 
-	public void ReleaseShot()
+	[Tooltip("The minimum duration between shots")]
+	public float Cooldown = 1;
+
+	[Tooltip("Whether this is currently cooling down from a previous shot")]
+	public bool IsCooling = false;
+
+	[Tooltip("Invoked when this has finished cooling down")]
+	public UnityEvent OnCooldownEnd = new();
+
+	public void EndCooldown()
 	{
+		this.IsCooling = false;
+		this.OnCooldownEnd.Invoke();
+	}
+
+	public void Shot()
+	{
+		if (this.IsCooling)
+			return;
+		this.IsCooling = true;
+		this.Invoke(nameof(this.EndCooldown), this.Cooldown);
 		var bullet_object = Instantiate(this.Bullet.gameObject);
 		var bullet = bullet_object.GetComponent<Bullet>();
-		var power = this.CurrentPower;
-		this.CurrentPower = 0f;
-		bullet.Shot(this, power, this.Aim.Direction.normalized);
-		this.OnShot.Invoke(power);
-	}
-	#endregion
-
-	[Tooltip("Whether this should be charging a shot")]
-	private bool _isCharging = false;
-	public bool IsCharging
-	{
-		get => this._isCharging;
-		set
-		{
-			var old = this._isCharging;
-			this._isCharging = value;
-			if (old && !value)
-				this.ReleaseShot();
-		}
-	}
-
-	private void Update()
-	{
-		this.UpdateCharge(Time.deltaTime);
+		bullet.Shot(this, this.Power, this.Aim.Direction.normalized);
+		this.OnShot.Invoke(this.Power);
 	}
 }
