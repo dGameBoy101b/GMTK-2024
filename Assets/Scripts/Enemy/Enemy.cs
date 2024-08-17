03645cc72230458d32def2b06b07feff73d8db27
dGameBoy101b <rhysmader@gmail.com>
2024-08-17 21:11:39 +0800

    Created enemy scripts.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	[Tooltip("What this should try to kill")]
+	public Transform Target;
+}
---
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+	[Tooltip("The move component to control")]
+	public Move Move;
+
+	[Tooltip("The enemy used to fetch the target")]
+	public Enemy Enemy;
+
+	public void UpdateDirection()
+	{
+		this.Move.CurrentDirection = this.Enemy.Target.position - this.Move.transform.position;
+	}
+
+	private void Update()
+	{
+		this.UpdateDirection();
+	}
+}
