917daed87902551ece227aeecaded3a960d5d8c8
dGameBoy101b <rhysmader@gmail.com>
2024-08-19 14:02:05 +0800

    Created integer branch script.

---
+++ Assets/Scripts/IntegerBranch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IntegerBranch : MonoBehaviour
+{
+	[Tooltip("The threshold to check")]
+	public int Threshold = 0;
+
+	[Flags]
+	public enum Mode
+	{
+		None,
+		Equal = 1,
+		NotEqual = 1 << 1,
+		GreaterThan = 1 << 2,
+		LessThan = 1 << 3,
+	}
+	public Mode CurrentMode;
+
+	[Tooltip("Invoked when the threshold is crossed.\nParameter is the value that crossed the threshold.")]
+	public UnityEvent<int> OnCrossThreshold = new();
+
+	public bool DoesCross(int value)
+	{
+		return (this.CurrentMode.HasFlag(Mode.Equal) && value == this.Threshold)
+			|| (this.CurrentMode.HasFlag(Mode.NotEqual) && value != this.Threshold)
+			|| (this.CurrentMode.HasFlag(Mode.GreaterThan) && value > this.Threshold)
+			|| (this.CurrentMode.HasFlag(Mode.LessThan) && value < this.Threshold);
+	}
+
+	public void Check(int value)
+	{
+		if (this.DoesCross(value))
+			this.OnCrossThreshold.Invoke(value);
+	}
+}
